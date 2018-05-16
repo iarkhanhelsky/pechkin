bc86442b60e3a1afb698cc3ba24acb7b9941bad7
iarkhanhelsky <ilya.arkhanhelsky@gmail.com>
2018-05-16 19:23:04 +0300

    Will generate anonymous class on each Pechkin::create

--- lib/pechkin.rb
+++ lib/pechkin.rb
@@ -4,40 +4,44 @@ require_relative 'pechkin/config'
 require_relative 'pechkin/telegram'
 
 module Pechkin # :nodoc:
-  # All endpoints are generated at runtime
-  class App < Grape::API
-    class << self
-      # Will create all endpoints based on configuration
-      # data
-      def configure(config)
-        p config
-        base_path = config['base_path']
-        resource base_path do
-          create_chanels(config['chanels'], config['bots'])
+  module Generator
+    def configure(config)
+      base_path = config['base_path']
+      resource base_path do
+        create_chanels(config['chanels'], config['bots'])
+      end
+      self
+    end
+
+    def create_chanels(chanels, bots)
+      chanels.each do |chanel_name, chanel_desc|
+        bot_token = bots[chanel_desc['bot']]
+        chat_ids = chanel_desc['chat_ids']
+        bot = Telegram::Chanel.new(bot_token, chat_ids)
+        resource chanel_name do
+          create_chanel(bot, chanel_desc)
         end
-        self
+      end
+    end
+
+    def create_chanel(bot, chanel_desc)
+      chanel_desc['messages'].each do |message_name, message_desc|
+        post message_name do
+          template = message_desc['template']
+          opts = { markup: 'HTML' }.update(message_desc['options'] || {})
+          bot.send_message(template, params, opts)
+        end
+      end
+    end
+  end
+
+  class << self
+    def create(config)
+      klazz = Class.new(Grape::API) do
+        extend Generator
       end
 
-      def create_chanels(chanels, bots)
-        chanels.each do |chanel_name, chanel_desc|
-          bot_token = bots[chanel_desc['bot']]
-          chat_ids = chanel_desc['chat_ids']
-          bot = Telegram::Chanel.new(bot_token, chat_ids)
-          resource chanel_name do
-            create_chanel(bot, chanel_desc)
-          end
-        end
-      end
-
-      def create_chanel(bot, chanel_desc)
-        chanel_desc['messages'].each do |message_name, message_desc|
-          post message_name do
-            template = message_desc['template']
-            opts = { markup: 'HTML' }.update(message_desc['options'] || {})
-            bot.send_message(template, params, opts)
-          end
-        end
-      end
+      klazz.configure(config)
     end
   end
 end
