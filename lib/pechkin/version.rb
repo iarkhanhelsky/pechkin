18a794d51ee304795567bbd4ebd9f660ab113f91
iarkhanhelsky <ilya.arkhanhelsky@gmail.com>
2018-12-03 14:31:41 +0300

    Updated logging configuration

--- lib/pechkin/version.rb
+++ lib/pechkin/version.rb
@@ -1,7 +1,7 @@
 module Pechkin
   # Keeps actual version
   module Version
-    VERSION = [0, 0, 4].freeze
+    VERSION = [0, 0, 5].freeze
     class << self
       def version_string
         VERSION.join('.')
