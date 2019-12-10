b46cc1c427d05112f8cfca3c6218ecc325bced5e
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2019-12-10 13:56:22 +0300

    Add support for "-" trim mode for ERB

--- spec/pechkin/message_template_spec.rb
+++ spec/pechkin/message_template_spec.rb
@@ -4,5 +4,11 @@ module Pechkin
       expect(MessageTemplate.new('Hello <%= name %>!').render(name: 'John'))
         .to eq('Hello John!')
     end
+
+    it 'Supports \'-\' trim mode' do
+      template = "Hello\n<% if false %> not printed <% end -%>\n"
+      expect(MessageTemplate.new(template).render({}))
+        .to eq("Hello\n")
+    end
   end
 end
