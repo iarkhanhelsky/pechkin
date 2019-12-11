f06e60aecd1a3ef485816e73e3c9e83b26c6cdea
Aleh Krutsikau <krolser@gmail.com>
2019-12-12 00:22:36 +0300

    Use module constant to calculate one time

--- lib/pechkin/message_template.rb
+++ lib/pechkin/message_template.rb
@@ -6,13 +6,15 @@ module Pechkin
     end
   end
 
+  ERB_INITIALIZE_KEYWORD_ARGUMENTS = ERB.instance_method(:initialize).parameters.assoc(:key)
+
   # Message template to render final message.
   class MessageTemplate
     def initialize(erb)
       # ERB#initialize has different signature starting from Ruby 2.6.*
       # See link:
       # https://github.com/ruby/ruby/blob/2311087/NEWS#stdlib-updates-outstanding-ones-only
-      if ERB.instance_method(:initialize).parameters.assoc(:key) # Ruby 2.6+
+      if ERB_INITIALIZE_KEYWORD_ARGUMENTS # Ruby 2.6+
         @erb_template = ERB.new(erb, trim_mode: '-')
       else
         safe_level = nil
