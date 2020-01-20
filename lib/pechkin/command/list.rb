5d9d83548055426b6241494bce0e32f528a2d975
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2020-01-20 22:10:11 +0300

    Move list command implementation to Pechkin::Command::List

--- lib/pechkin/command/list.rb
+++ lib/pechkin/command/list.rb
@@ -7,7 +7,33 @@ module Pechkin
       end
 
       def execute
-        configuration.list
+        cfg = configuration
+
+        puts "Working dir: #{cfg.working_dir}"
+        print_bots(cfg.bots)
+        print_channels(cfg.channels)
+      end
+
+      private
+
+      def print_bots(bots)
+        puts "\nBots:"
+        bots.each do |name, bot|
+          puts "  #{name}(#{bot.connector}): #{bot.token}"
+        end
+      end
+
+      def print_channels(channels)
+        puts "\nChannels:"
+        channels.each do |channel_name, channel|
+          puts "  - name #{channel_name}"
+          puts "    bot: #{channel.connector.name}"
+          puts '    messages: '
+          channel.messages.each do |message_name, _message|
+            puts "     - /#{channel_name}/#{message_name}"
+          end
+          puts
+        end
       end
     end
   end
--- lib/pechkin/configuration.rb
+++ lib/pechkin/configuration.rb
@@ -79,24 +79,5 @@ module Pechkin
       @views = views
       @channels = channels
     end
-
-    def list
-      puts "Working dir: #{working_dir}\nBots:"
-
-      bots.each do |name, bot|
-        puts "  #{name}(#{bot.connector}): #{bot.token}"
-      end
-
-      puts "\nChannels:"
-      channels.each do |channel_name, channel|
-        puts "  - name #{channel_name}"
-        puts "    bot: #{channel.connector.name}"
-        puts '    messages: '
-        channel.messages.each do |message_name, _message|
-          puts "     - /#{channel_name}/#{message_name}"
-        end
-        puts
-      end
-    end
   end
 end
