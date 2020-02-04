9bea0b8b26df849a3f98d84bf14911d47e4c88fb
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2020-02-04 12:57:05 +0300

    Bump version

--- lib/pechkin/version.rb
+++ lib/pechkin/version.rb
@@ -1,7 +1,7 @@
 module Pechkin
   # Keeps actual version
   module Version
-    VERSION = [1, 2, 0].freeze
+    VERSION = [1, 2, 1].freeze
     class << self
       def version_string
         VERSION.join('.')
