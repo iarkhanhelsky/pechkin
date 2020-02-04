626567d9b40a2c015ab0ef34d472c3cbc553f571
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2020-02-04 23:05:51 +0300

    Added MessageMatcher spec

--- lib/pechkin/message_matcher.rb
+++ lib/pechkin/message_matcher.rb
@@ -1,11 +1,22 @@
 module Pechkin
+  class MessageMatchError < StandardError; end
   # Allows to match message configuration against received data.
   #
   # Data is checked againts either allow or forbid rules. But not both at the
   # same time.
   class MessageMatcher
-    def matches?(_message_config, _data)
+    def matches?(message_config, _data)
+      check(message_config)
+
       true
+    end
+
+    private
+
+    def check(message_config)
+      return unless message_config.key?('allow') && message_config.key?('forbid')
+
+      raise MessageMatchError, 'Both allow and forbid present in message config'
     end
   end
 end
