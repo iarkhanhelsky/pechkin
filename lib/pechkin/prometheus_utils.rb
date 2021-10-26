5dc5942beba3d2e3c24e57f400b41a770d199d6c
Ilya Arkhanhelsky <ilya.arkhanhelsky@vizor-games.com>
2021-10-26 14:03:18 +0300

    Add pechkin_version metric

--- lib/pechkin/prometheus_utils.rb
+++ lib/pechkin/prometheus_utils.rb
@@ -5,6 +5,12 @@ module Pechkin
         registry = ::Prometheus::Client.registry
         registry.gauge(:pechkin_start_time_seconds,
                        docstring: 'Startup timestamp').set(Time.now.to_i)
+
+        version_labels = { version: Pechkin::Version.version_string }
+        registry.gauge(:pechkin_version,
+                       docstring: 'Pechkin version', labels: [:version])
+                .set(1, labels: version_labels)
+
         registry
       end
     end
