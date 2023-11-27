b47487d80a7b8c12452bcac1afe96ded9f80f68d
Anton Rudy <anton.rudy@vizor-games.com>
2023-11-27 20:49:01 +0200

    Change comment language 2

--- lib/pechkin/command/run_server.rb
+++ lib/pechkin/command/run_server.rb
@@ -12,8 +12,8 @@ module Pechkin
         # Configure Puma server instead config.ru
         puma_config = Puma::Configuration.new do |user_config|
           user_config.bind "tcp://#{options.bind_address}:#{options.port}"
-          user_config.workers options.server_workers  # Установка количества воркеров
-          user_config.threads options.min_threads, options.max_threads # Установка минимума и максимума потоков
+          user_config.workers options.server_workers  # Set number of workers
+          user_config.threads options.min_threads, options.max_threads # Set max and min threads
           user_config.app AppBuilder.new.build(handler, options)
         end
 
