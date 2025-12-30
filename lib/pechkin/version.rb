2afa1bfeb568ada62fd3592ab0f168b8a06742fa
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2025-12-30 14:05:53 +0100

    v 2.1.3

--- lib/pechkin/version.rb
+++ lib/pechkin/version.rb
@@ -1,7 +1,7 @@
 module Pechkin
   # Keeps actual version
   module Version
-    VERSION = [2, 1, 0].freeze
+    VERSION = [2, 1, 3].freeze
     class << self
       def version_string
         VERSION.join('.')
